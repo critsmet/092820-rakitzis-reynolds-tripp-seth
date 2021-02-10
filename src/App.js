bd8d5cf727dd3375437dfa38524bf0da212557ab
critsmet <chrismetzgr@gmail.com>
2021-02-10 14:06:09 -0600

    split these into separate components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import React from 'react';
 import './App.css';
 
+let url = "https://www.googleapis.com/books/v1/volumes?q="
+
 class App extends React.Component {
 
-  state = {todo: '', todos: []}
+  state = {volumes: [], alphabetize: false}
 
-  handleChange = (event) => {
-    //the arrow function determines the 'this' keyword where it's define, in this case this arrow function is for an instance of the App component
-    if (this.checkForCharacters(event.target.value)){
-      this.setState({todo: event.target.value})
-    }
-  }
-
-  checkForCharacters(string){
-    return string.includes("0") ? false : true
-  }
-
-  handleSubmit = (event) => {
+  submitBookSearch = (event) => {
     event.preventDefault()
-    this.setState((upToDateState) => ({todos: [...upToDateState.todos, upToDateState.todo], todo: ''}), () => console.log(this.state))
+    let query = event.target.term.value
+    // this.setState({query}, () => console.log(this.state))
+    fetch(url + query)
+      .then(res => res.json())
+      .then(respObj => {
+        this.setState({volumes: respObj.items.map(volume => volume.volumeInfo.title)}, () => console.log(this.state))
+      })
+    event.target.reset()
   }
 
-  listTodos(){
-    let todos = this.state.todos
-    return todos.map(todo => <li key={todo}>{todo}</li>)
+  toggleAlphabetize = (event) => {
+    this.setState(prevState => ({alphabetize: !prevState.alphabetize}), () => console.log(this.state))
+  }
+
+  renderBooks = () => {
+    if (this.state.alphabetize){
+      return [...this.state.volumes].sort().map(book => <li key={book}>{book}</li>)
+    } else {
+      return this.state.volumes.map(book => <li key={book}>{book}</li>)
+    }
   }
 
   render(){
     return(
       <div className="App">
-        <div id="todo-form-container">
-          <form onSubmit={this.handleSubmit} id="todo-form">
-            <input onChange={this.handleChange} name="todo" type="text" value={this.state.todo}/>
-            <input type="submit" value="Add Todo!" />
-          </form>
-        </div>
-        <div id="todos-container">
-          <h2>Todos Go Here</h2>
-          <ul id="todo-list">
-            {this.listTodos()}
+        <form onSubmit={this.submitBookSearch}>
+          <input type="text" name="term" />
+          <input type="submit" value="Search for Books!" />
+        </form>
+        <br/>
+        <div id="books-container">
+          <input onChange={this.toggleAlphabetize} type="checkbox" name="sort"/>
+          <label>Sort Alphabetically</label>
+          <p>{this.state.alphabetize ? "TRUE" : "FALSE"}</p>
+          <ul>
+            {this.renderBooks()}
           </ul>
         </div>
       </div>
